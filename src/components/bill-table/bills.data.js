5fd47fa18f447831ac2e479f36eda7b529a87e68
MagdalenaJasmine <magdalenacooper92@gmail.com>
2021-03-04 16:04:55 -0800

    updated dummy data to reflect needs

--- src/components/bill-table/bills.data.js
+++ src/components/bill-table/bills.data.js
@@ -3,6 +3,7 @@ const BILL_DATA = [
   { id: 2, name: `Bill of Weed`, stage: `not passed` },
   { id: 3, name: "MJ Bill", stage: `in progress` },
   { id: 4, name: "Sativa bill", stage: `passed` },
+  { id: 4, name: "Bill de Sativa", stage: `passed` },
 ];
 
 export default BILL_DATA;
